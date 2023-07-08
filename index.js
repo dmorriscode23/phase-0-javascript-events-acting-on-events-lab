5e2dcf1e2831e7286458e09a4e77b7bdcf6a8172
Davian Morris <davianmorris35@gmail.com>
2023-07-08 01:12:33 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Your code here
+
+
 function moveDodgerLeft() {
   const leftNumbers = dodger.style.left.replace("px", "");
   const left = parseInt(leftNumbers, 10);
@@ -13,3 +15,12 @@ document.addEventListener("keydown", function (e) {
     moveDodgerLeft();
   }
 });
+
+function moveDodgerRight() {
+  var dodger = document.getElementById('dodger');
+  var currentPosition = parseInt(dodger.style.left) || 0; 
+  var newPosition = currentPosition + 10; 
+  
+  dodger.style.left = newPosition + 'px'; 
+}
+  
